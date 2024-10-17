55ef73bc0c364d352e51bc3405faa207d86da055
Maggie <mheathco@ucsc.edu>
2024-10-17 02:10:40 -0700

    better popup

--- app_ui/tabs/Home.js
+++ app_ui/tabs/Home.js
@@ -1,26 +1,62 @@
 // Home.js
 import React from 'react';
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { View, Text, TouchableOpacity, TouchableWithoutFeedback, Animated, Alert   } from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
 
 import colors from '../style/colors';
 import styles from '../style/styles';
 
 
-const HomeDisplay = () => (
-  <View style={styles.screen}>
-    {/* Add Chore Button */}
-    <TouchableOpacity
-      style={styles.button}
-      onPress={() => alert('Plus Icon Pressed!')}
-      activeOpacity={0.7}  // opacity when pressed
-    >
-      <Icon name="add" size={40} color={colors.white} />
-    </TouchableOpacity>
-    <Text style={styles.buttonDescription}>
-      add chore
-    </Text>
-  </View>
-);
+const HomeDisplay = () => {
+  const scale = React.useRef(new Animated.Value(1)).current;
+
+  const handlePressIn = () => {
+    Animated.timing(scale, {
+      toValue: 0.8, // scale down to 80%
+      duration: 150,
+      useNativeDriver: true,
+    }).start();
+  };
+
+  const handlePressOut = () => {
+    Animated.timing(scale, {
+      toValue: 1, // scale back to original size
+      duration: 150,
+      useNativeDriver: true,
+    }).start();
+  };
+
+  const showAlert = () => {
+    Alert.alert(
+      'How would you like to add a chore?',
+      '',
+      [
+        {
+          text: 'Create custom chore',
+          onPress: () => console.log('custom'),
+        },
+        {
+          text: 'Edit from preset list',
+          onPress: () => console.log('preset'),
+        },
+      ],
+      { cancelable: true } // allow dismissing by tapping outside
+    );
+  };
+
+  return (
+    <View style={styles.screen}>
+      {/* Add Chore Button */}
+      <TouchableWithoutFeedback onPress={showAlert} onPressIn={handlePressIn} onPressOut={handlePressOut}>
+        <Animated.View style={[styles.button, { transform: [{ scale }] }]}>
+          <Icon name="add" size={40} color="#fff" />
+        </Animated.View>
+      </TouchableWithoutFeedback>
+      <Text style={styles.buttonDescription}>
+        add chore
+      </Text>
+    </View>
+  );
+};
 
 export default HomeDisplay;
