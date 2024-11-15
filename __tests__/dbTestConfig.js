a330a1e8e2507572684f726620588eb9338452b6
annabel <anptruon@ucsc.edu>
2024-11-15 11:26:30 -0800

    fixing styles.js, package-lock, and package

--- __tests__/dbTestConfig.js
+++ __tests__/dbTestConfig.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2');
 
 const db = mysql.createConnection({
-    host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || '',
-    database: process.env.DB_DATABASE || 'choremates_test',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
 });
 
 module.exports = db;
